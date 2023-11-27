394131a8b8f9977a86ad6ee6ebb1cd2149e185ca
mugisha-blaine <n.blaine@alustudent.com>
2023-11-27 05:14:18 -0500

    books

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,12 +6,19 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-return users.some(user => user.username === username && user.password === password);
+    //write code to check is the username is valid
+    return username.trim().length > 0;
 };
 
+const authenticatedUser = (username, password) => {
+    // Assuming users are stored in an array of objects with 'username' and 'password' properties
+    const foundUser = users.find(user => user.username === username && user.password === password);
+  
+    return !!foundUser; // Returns true if user is found, false otherwise
+  };
+  
 // Only registered users can login
-regd_users.post("/customer/login", (req, res) => {
+regd_users.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   // Check if the provided username is valid
@@ -37,8 +44,34 @@ regd_users.post("/customer/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, token } = req.user; // Retrieve username and token from the session (assuming it exists)
+  const { review } = req.query; // Get review from the request query
+  const isbn = req.params.isbn; // Get ISBN from the URL parameters
+
+  // Find the book by ISBN in the books database (assuming books is an array of objects)
+  const bookIndex = books.findIndex(book => book.isbn === isbn);
+
+  if (bookIndex === -1) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  // Check if the user has already posted a review for this ISBN
+  const existingReviewIndex = books[bookIndex].reviews.findIndex(
+    rev => rev.username === username
+  );
+
+  if (existingReviewIndex !== -1) {
+    // If the user has already posted a review, modify the existing one
+    books[bookIndex].reviews[existingReviewIndex].review = review;
+    return res.status(200).json({ message: "Review modified successfully" });
+  } else {
+    // If the user has not posted a review for this ISBN, add a new review
+    books[bookIndex].reviews.push({ username, review });
+    return res.status(200).json({ message: "Review added successfully" });
+  }
 });
+ 
+ 
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
